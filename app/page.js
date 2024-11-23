035261545b89485b5c5e75378dd32fd1fa34878c
dasashes713 <dasashes713@gmail.com>
2024-11-23 19:37:43 +0530

    Update page.js

--- app/page.js
+++ app/page.js
@@ -5,6 +5,7 @@ import Navbar from "@/components/Navbar";
 export default function Home() {
   return (
     <div>
+    
        {/* <Navbar /> */}
       <div className="container mx-auto p-4">
         <h1 className="text-2xl font-bold">Welcome to Kiddospace!</h1>
